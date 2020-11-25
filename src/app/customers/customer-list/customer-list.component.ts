ea730942c1d87560dd6348bdb51e07f6e6437aaf
pavel asadchy <asadchy.pavel@gmail.com>
2020-11-26 00:33:30 +0300

    feat: selectors

--- src/app/customers/customer-list/customer-list.component.ts
+++ src/app/customers/customer-list/customer-list.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { Store } from '@ngrx/store';
+import { Store, select } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import { Customer } from 'src/app/shared/models/customer.model';
+import { AppState } from 'src/app/shared/stores/app.state';
 import { LOAD_CUSTOMERS_ALL_ACTION } from 'src/app/shared/stores/customer/customer.actions';
+import * as fromCustomer from '../../shared/stores/customer/customer.selector';
 
 @Component({
   selector: 'app-customer-list',
@@ -9,15 +13,13 @@ import { LOAD_CUSTOMERS_ALL_ACTION } from 'src/app/shared/stores/customer/custom
 })
 export class CustomerListComponent implements OnInit {
 
-  customers$;
+  customers$: Observable<Customer[]>;
 
-  constructor(private store: Store<any>) { }
+  constructor(private store: Store<AppState>) { }
 
   ngOnInit(): void {
     this.store.dispatch(LOAD_CUSTOMERS_ALL_ACTION());
-    this.store.subscribe(state => {
-      this.customers$ = state.customers.customers;
-    });
+    this.customers$ = this.store.pipe(select(fromCustomer.GET_CUSTOMERS_SELECTOR));
   }
 
 }
---
+++ src/app/shared/stores/customer/customer.selector.ts
@@ -0,0 +1,29 @@
+import { createSelector, createFeatureSelector} from '@ngrx/store';
+import { CustomerState } from './customer.state';
+
+export const FEATURE_KEY = 'customers';
+
+export const SELECT_CUSTOMER_FEATURE = createFeatureSelector<CustomerState>(
+  FEATURE_KEY
+);
+
+export const GET_CUSTOMERS_SELECTOR = createSelector(
+  SELECT_CUSTOMER_FEATURE,
+  (state: CustomerState) => state.customers
+);
+
+export const GET_CUSTOMERS_LOADING_SELECTOR = createSelector(
+  SELECT_CUSTOMER_FEATURE,
+  (state: CustomerState) => state.loading
+);
+
+export const GET_CUSTOMERS_LOADED_SELECTOR = createSelector(
+  SELECT_CUSTOMER_FEATURE,
+  (state: CustomerState) => state.loaded
+);
+
+export const GET_CUSTOMERS_ERROR_SELECTOR = createSelector(
+  SELECT_CUSTOMER_FEATURE,
+  (state: CustomerState) => state.error
+);
+
