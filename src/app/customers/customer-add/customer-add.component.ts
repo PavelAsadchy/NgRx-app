4e68f80f94099b591f5601d795842afcfb0740b1
pavel asadchy <asadchy.pavel@gmail.com>
2020-11-28 19:33:55 +0300

    feat: create customer component

--- src/app/customers/customer-add/customer-add.component.ts
+++ src/app/customers/customer-add/customer-add.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Store } from '@ngrx/store';
+import { Customer } from 'src/app/shared/models/customer.model';
+import { AppState } from 'src/app/shared/stores/app.state';
+import { CREATE_CUSTOMER_ACTION } from 'src/app/shared/stores/customer/customer.actions';
+import { CustomerState } from 'src/app/shared/stores/customer/customer.state';
 
 @Component({
   selector: 'app-customer-add',
@@ -7,9 +13,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CustomerAddComponent implements OnInit {
 
-  constructor() { }
+  customerForm: FormGroup;
+
+  constructor(
+    private fb: FormBuilder,
+    private store: Store<AppState>
+  ) { }
 
   ngOnInit(): void {
+    this.customerForm = this.fb.group({
+      name: ['', Validators.required],
+      phone: ['', Validators.required],
+      address: ['', Validators.required],
+      membership: ['', Validators.required],
+    });
+  }
+
+  createCustomer() {
+    const newCustomer: Customer = {
+      name: this.customerForm.get('name').value,
+      phone: this.customerForm.get('phone').value,
+      address: this.customerForm.get('address').value,
+      membership: this.customerForm.get('membership').value,
+    };
+
+    this.store.dispatch(CREATE_CUSTOMER_ACTION({ newCustomer }));
+
+    this.customerForm.reset();
   }
 
 }
